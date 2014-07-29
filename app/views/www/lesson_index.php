54e9d23d1a39c08e62bc7ae8169bfe65429f1c6d
dyong <dyong525@163.com>
2014-07-29 16:09:55 +0800

    添加图片

--- app/views/www/lesson_index.php
+++ app/views/www/lesson_index.php
@@ -14,8 +14,9 @@
   		echo "<li><a href='?control=lesson&id=$value[catid]'>$value[catname]</a></li>" ;
   	}
   ?>
-  <li><a href='?control=order&catid=75'><img src="images/lkbm.jpg" border="0" width="100" height="36" align=order></a></li>
   </ul>
+  <div>&nbsp;</div>
+  <div><a href='?control=order&catid=75'><img src="images/lkbm.jpg" border="0" width="105" height="35" align=order></a></div>
   </div>
 
   <!--left end-->
