29178029f74977176c46fe4084a72983605550ac
dyong <dyong525@163.com>
2014-08-04 22:37:33 +0800

    cnzz

--- app/views/www/comm-footer.php
+++ app/views/www/comm-footer.php
@@ -3,7 +3,7 @@
 	&nbsp;&nbsp;<a href="http://club.nyipcn.com" target="_blank"><img src="images/ad1.jpg" width="410" height="110" border=0 style="margin-top: 5px;top:5px;"></a>&nbsp;&nbsp;
 	<a href="http://www.newshootedu.com/" target="_blank"><img src="images/ad2.jpg" width="520" height="110" border=0 style="margin-top: 5px;top:5px;"></a>
   <div class="ifooter_1"><a href="http://club.nyipcn.com" target="_blank">纽摄俱乐部</a> |<a href="http://www.newshootedu.com/" target="_blank">纽摄教育在线学院</a> | <a href="index.php?control=info" target="_blank">联系我们</a></div>
-  <div class="ifooter_2">北京纽摄教育科技有限公司版权所有，严禁转载  Copyright © 2013 · All Right Reserved<br>京公网安备110108003932号&nbsp;&nbsp;京ICP备05076531号&nbsp;&nbsp;&nbsp;&nbsp;<script src="http://v1.cnzz.com/stat.php?id=5642382&web_id=5642382&show=pic" language="JavaScript"></script>
+  <div class="ifooter_2">北京纽摄教育科技有限公司版权所有，严禁转载  Copyright © 2013 · All Right Reserved<br>京公网安备110108003932号&nbsp;&nbsp;京ICP备05076531号&nbsp;&nbsp;&nbsp;&nbsp;<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5354854'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/stat.php%3Fid%3D5354854%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
   <a href="index.php?control=detail&tid=59&id=633" target="_blank">友情链接</a></div>
 </div>
 <br/>
